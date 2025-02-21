772d82c6fad1f5cb10815f838c06e9a9f8f4e08f
donbentley <donbentley@uga.edu>
2025-02-21 13:33:31 -0500

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./containers/**/*.{js,ts,jsx,tsx,mdx}"],
+	content: ["./src/**/*.{html,js}","./**/*.{html,js}", "./containers/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {},
 	},
